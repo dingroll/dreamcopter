4e8741aa3c06cf45cdad3e5dd81e6e39668dcce9
Stuart P. Bentley <stuart@testtrack4.com>
2015-12-28 19:19:19 +0000

    style and color message widgets

--- partywall-styles.js
+++ partywall-styles.js
@@ -4,16 +4,32 @@ var styler = partywallStyler();
 // layout
 
 styler.addStyle([
-  ['.slack-message','#source-area'], ['.slack-message','#source-details']],
-  'flex: none; display: flex; flex-flow: column;');
+  ['.slack-message','#source-area'],
+  ['.slack-message','#source-details']],
+    'flex: none; display: flex; flex-flow: column;');
 styler.addStyle([
   ['.slack-message','#description-line']],
-  'flex: none; display: flex; flex-flow: row; '+
-  'justify-content: space-between;');
+    'flex: none; display: flex; flex-flow: row; '+
+    'justify-content: space-between;');
 styler.addStyle([
-  ['.dingroll-message','select'], ['.dingroll-message','button']],
-  'flex: none;');
+  ['.dingroll-message','select'],
+  ['.dingroll-message','button']],
+    'flex: none;');
 styler.addStyle([['.dingroll-message','#message-tags']],
-  'flex: 1;');
-
+    'flex: 1;');
+styler.addStyle([
+  ['.slack-message','#description-line']],
+    'margin: 4px 8px 0;');
 // style
+
+styler.addStyle([
+  ['.slack-message','button']],
+    'background: #0bf;');
+styler.addStyle([
+  ['.slack-message','.new-dingroll-message button']],
+    'background: #fb2;');
+styler.addStyle([
+  ['.slack-message','#username'],
+  ['.slack-message','#timestamp']],
+    'color: white;');
+
